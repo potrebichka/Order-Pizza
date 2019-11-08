c9afb55099b21291c7b3b2049cbb3472b6a506a7
Nina Potrebich <potrebich@gmail.com>
2019-11-08 09:05:39 -0800

    add back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+function Pizza() {
+    this.toppings = [],
+    this.size = 0
+}
+
+Pizza.prototype.addTopping = function (topping) {
+    this.toppings.push(topping);
+}
+
+Pizza.prototype.changeSize = function (size) {
+    this.size = size;
+}
+
+Pizza.prototype.calculatePrice = function() {
+    var price = 0;
+    return price
+}
+
+
 $(document).ready(function() {
 
 
