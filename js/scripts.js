26c2059b633c52cd2f0d2b2fc2ba23551d5ec9b5
Nina Potrebich <potrebich@gmail.com>
2019-11-08 10:34:45 -0800

    add more variations for pizza in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
-function Pizza() {
-    this.toppings = [],
-    this.size = 0
+function Pizza(toppings, size) {
+    this.toppings = toppings,
+    this.size = size,
+    this.price = 0
 }
 
-Pizza.prototype.addTopping = function (topping) {
-    this.toppings.push(topping);
-}
+// Pizza.prototype.addTopping = function (topping) {
+//     this.toppings.push(topping);
+// }
 
-Pizza.prototype.changeSize = function (size) {
-    this.size = size;
-}
+// Pizza.prototype.changeSize = function (size) {
+//     this.size = size;
+// }
 
 Pizza.prototype.calculatePrice = function() {
-    var price = 0;
-    return price
+    console.log(this.toppings, this.size)
+    return this.price
 }
 
 
 $(document).ready(function() {
-
+    $("#pizzaForm").submit(function(event) {
+        event.preventDefault();
+        var chosenToppings = [];
+        $("input:checkbox[name=toppings]:checked").each(function() {
+            chosenToppings.push($(this).val());
+        })
+        var chosenSize = $("#sizes").val();
+        var pizza = new Pizza(chosenToppings, chosenSize);
+        pizza.calculatePrice();
+    })
 
 
 });
